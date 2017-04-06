9b8faf38ea82615f6afb8aee5bf014ba895e02fb
Olivier Fambon <olivier.fambon@inria.fr>
2017-04-06 17:40:58 +0200

    tests: add test_pollution_download

--- tests/test_downloads.py
+++ tests/test_downloads.py
@@ -1,6 +1,7 @@
 import pytest
 
 import embers.datasets.citypulse.traffic as traffic
+import embers.datasets.citypulse.pollution as pollution
 import os
 
 
@@ -12,7 +13,22 @@ def test_traffic_download(monkeypatch):
     dest_dir = "traffic_june_sep"
     metadata = "trafficMetaData.csv"
 
+    check_dataset(dest_dir, metadata)
+
+
+def check_dataset(dest_dir, metadata):
     assert os.path.isdir(dest_dir)
     assert len(os.listdir(dest_dir)) == 449
     assert os.path.isfile(metadata)
     assert len(open(metadata).read()) == 103917
+
+
+def test_pollution_download(monkeypatch):
+    x = pollution.Pollution()
+    x.download()
+    monkeypatch.chdir("embers.datasets.citypulse")
+
+    dest_dir = "pollution"
+    metadata = "trafficMetaData.csv"
+
+    check_dataset(dest_dir, metadata)
