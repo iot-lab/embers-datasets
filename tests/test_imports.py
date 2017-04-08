4c31d56d718ac0c9f372f98b65a683e097db2ee3
Olivier Fambon <olivier.fambon@inria.fr>
2017-04-09 01:05:59 +0200

    tests: add test_citypulse import tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,3 +5,21 @@ def test_citypulse_traffic():
     import embers.datasets.citypulse.traffic as traffic
     x = traffic.Traffic()
     x.download # just check this exists, don't run it
+
+
+@pytest.mark.parametrize("events", [
+    "traffic",
+    "pollution",
+])
+def test_citypulse(events):
+    x = get_dataset("citypulse", events)
+
+    x.download # just check this exists, don't run it
+    x.get_source
+
+
+def get_dataset(dataset, event_type):
+    package_name = "embers.datasets." + dataset + "." + event_type
+    class_name = event_type.capitalize()
+    package = __import__(package_name, fromlist=[class_name])
+    return package.__dict__[class_name]()
